317465e636271a91a3280a52bd1a29423e56a044
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-16 21:56:04 +0000

    correct images are displayed

--- controllers/football_controllers.rb
+++ controllers/football_controllers.rb
@@ -9,21 +9,21 @@ class FootballControllers < Sinatra::Base
 
   $photos = [{
 	 id: 0,
-	 title: "Photo 1",
-	 body: "This is the first post",
-   img: "http://fillmurray.com/300/300"
+	 title: "Ronaldo el phenomeno",
+	 body: "Basic Description",
+   img: "http://inyarwanda.com/img/201509/logos/1442558788_ronaldo.jpg"
 },
 {
     id: 1,
-    title: "Photo 2",
-    body: "This is the second post",
-    img: "http://fillmurray.com/300/300"
+    title: "Cristiano Ronaldo",
+    body: "Basic Description",
+    img: "https://www.diariogol.com/uploads/s1/52/44/81/3/20170926-636420589232313227_20170926214612-kR5C--980x554%40MundoDeportivo-Web_16_643x397.jpg"
 },
 {
     id: 2,
-    title: "Photo 3",
-    body: "This is the third post",
-    img: "http://fillmurray.com/300/300"
+    title: "Lionel messi",
+    body: "Basic Description",
+    img: "https://cdn.images.express.co.uk/img/dynamic/67/590x/Lionel-Messi-782258.jpg"
 }];
 
   get "/" do
