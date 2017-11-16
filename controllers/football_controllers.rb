4d5e4eae928303c1b8de130e741e4b502a90fabb
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-16 19:56:29 +0000

    linked my views folder to the directory

--- controllers/football_controllers.rb
+++ controllers/football_controllers.rb
@@ -1,14 +1,17 @@
 class FootballControllers < Sinatra::Base
 
-  # set :root, File.join(File.dirname(__FILE__),"..")
-  # set :views, Proc.new { File.join(root, "views") }
+  set :root, File.join(File.dirname(__FILE__),"..")
+  set :views, Proc.new { File.join(root, "views") }
 
   configure :development do
     register Sinatra::Reloader
   end
 
+
+
   get "/" do
-    "Hello welcome to my website, I work"
+    @title = "Homepage"
+    erb :'footballers/home'
   end
 
 
